c577dd7d7096f57e64cbe5c35e48434e298d8cf9
Oizys18 <oizys18@gmail.com>
2020-08-17 17:39:30 +0900

    Add index

---
+++ src/components/views/Index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import index from "../../assets/styles/index.scss";
+
+
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div class="Wrapper">
+        <div class="Container"></div>
+      </div>
+    );
+  }
+}
+
+export default Index;
