708b4998269331236007ea2d69f10ad3a8a2a54f
Oizys18 <oizys18@gmail.com>
2020-09-09 23:07:58 +0900

    Add rest back

---
+++ BACK/articles/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=140)
+    content = models.TextField()
+    image = models.ImageField(blank=True)
+    video = models.CharField(max_length=12,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+        
