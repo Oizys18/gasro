4c854111edb0146d6bc4aebc5f89e420c17364b3
Oizys18 <oizys18@gmail.com>
2020-09-28 18:10:09 +0900

    Mod index

--- FRONT/src/index.jsx
+++ FRONT/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Root from "routes/Root";
 import ReactDOM from "react-dom";
 import Theme from "assets/styles/Theme";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import GlobalStyles from "assets/styles/GlobalStyles";
 ReactDOM.render(
   <React.StrictMode>
