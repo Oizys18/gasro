510d14f6e8cca4212741bc8eac72dedc6fb98be3
Oizys18 <oizys18@gmail.com>
2020-11-20 17:04:54 +0900

    Del unused backend files

--- BACK/articles/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post  # 모델 설정
-        fields = ('id', 'title', 'content','created_at','image','video' )  # 필드 설정
-        # read_only_fields = ('created_at',)
