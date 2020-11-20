364cb9d6afce877c7a32e66d003d14f5f8376d46
Oizys18 <oizys18@gmail.com>
2020-11-20 18:38:39 +0900

    Add new front client

---
+++ FRONT/src/components/common/buttons/BasicButton.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import Button from "react-bootstrap/Button";
+
+export class BasicButton extends Component {
+  static propTypes = {};
+
+  render() {
+    return;
+    <Button variant="primary"></Button>;
+  }
+}
+
+export default BasicButton;
