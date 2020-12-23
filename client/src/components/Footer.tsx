eac1920a9139ea67e830c3df9d7db5f3a2e0e404
Oizys18 <oizys18@gmail.com>
2020-12-23 22:50:24 +0900

    Add Footer components

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -8,7 +8,10 @@ function Footer({ ...props }: Props) {
     <>
       <FooterWrapper>
         <Container>
-          <div>Footer</div>
+          <FooterContainer>
+            <Contacts>Contacts</Contacts>
+            <Info>Info</Info>
+          </FooterContainer>
         </Container>
       </FooterWrapper>
     </>
@@ -17,9 +20,12 @@ function Footer({ ...props }: Props) {
 export default Footer;
 
 const FooterWrapper = styled.div`
-  position: fixed;
   bottom: 0;
   width: 100%;
-  height: 30px;
+  height: 50px;
   background-color: grey;
 `;
+
+const FooterContainer = styled.div``;
+const Contacts = styled.div``;
+const Info = styled.div``;
