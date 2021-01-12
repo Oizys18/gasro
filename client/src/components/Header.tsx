a4b6660fcb31233699e45a6bb5dbde3c87ecf07f
Oizys18 <oizys18@gmail.com>
2021-01-12 23:35:26 +0900

    Header router

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 interface Props {}
 export default function Header({ ...props }: Props) {
-  // const [focus, setFocus] = useState<number>(0);
   const location = useLocation();
-  console.log(location.pathname);
+
   useEffect(() => {
+    // 모바일 햄버거 toggle
     const toggle = document.getElementById("nav-toggle"),
       nav = document.getElementById("nav-menu");
     if (toggle && nav) {
@@ -13,6 +13,25 @@ export default function Header({ ...props }: Props) {
         nav.classList.toggle("show-menu");
       });
     }
+    // 메뉴 포커스
+    // switch (location.pathname) {
+    //   case "/":
+    //     const company = document.getElementById("company");
+    //     company?.classList.add("active-link");
+    //     break;
+    //   case "/products":
+    //     const products = document.getElementById("products");
+    //     products?.classList.add("active-link");
+    //     break;
+    //   case "/notice":
+    //     const notice = document.getElementById("notice");
+    //     notice?.classList.add("active-link");
+    //     break;
+    //   case "/support":
+    //     const support = document.getElementById("support");
+    //     support?.classList.add("active-link");
+    //     break;
+    // }
   });
 
   return (
@@ -23,35 +42,67 @@ export default function Header({ ...props }: Props) {
       <header className="l-header" id="header">
         <nav className="nav bd-container">
           <Link to="/">
-            <div className="nav__logo">Gastec Korea</div>
+            <h2 className="nav__logo">Gastec Korea</h2>
           </Link>
 
           <div className="nav__menu" id="nav-menu">
             <ul className="nav__list">
               <li className="nav__item">
                 <Link to="/">
-                  <a href="#home" className="nav__link active-link">
+                  <a
+                    href="#home"
+                    className={
+                      location.pathname == "/"
+                        ? "nav__link active-link"
+                        : "nav__link"
+                    }
+                    id="company"
+                  >
                     회사소개
                   </a>
                 </Link>
               </li>
               <li className="nav__item">
                 <Link to="/products">
-                  <a href="#home" className="nav__link">
+                  <a
+                    href="#home"
+                    className={
+                      location.pathname == "/products"
+                        ? "nav__link active-link"
+                        : "nav__link"
+                    }
+                    id="products"
+                  >
                     제품소개
                   </a>
                 </Link>
               </li>
               <li className="nav__item">
                 <Link to="/notice">
-                  <a href="#home" className="nav__link">
+                  <a
+                    href="#home"
+                    className={
+                      location.pathname == "/notice"
+                        ? "nav__link active-link"
+                        : "nav__link"
+                    }
+                    id="notice"
+                  >
                     공지사항
                   </a>
                 </Link>
               </li>
               <li className="nav__item">
                 <Link to="/support">
-                  <a href="#home" className="nav__link">
+                  <a
+                    href="#home"
+                    className={
+                      location.pathname == "/support"
+                        ? "nav__link active-link"
+                        : "nav__link"
+                    }
+                    id="support"
+                  >
                     고객지원
                   </a>
                 </Link>
