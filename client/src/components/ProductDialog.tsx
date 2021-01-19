f8cc26c3c2bc18e8f21c88126d1b1a30ec8c77c7
Oizys18 <oizys18@gmail.com>
2021-01-19 23:05:48 +0900

    Mod dialog

--- client/src/components/ProductDialog.tsx
+++ client/src/components/ProductDialog.tsx
@@ -97,11 +97,7 @@ export default function CustomizedDialogs({ ...props }: DialogProps) {
         </IconButton>
         <DialogContent dividers>
           <img src={props.imgSrc} alt="" />
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </Typography>
+
           <Typography gutterBottom>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
             Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
