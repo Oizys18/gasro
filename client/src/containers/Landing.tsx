005335951c8572bc2af4f59143a08103056957be
Oizys18 <oizys18@gmail.com>
2021-02-17 05:08:52 +0900

    Mod landing page

--- client/src/containers/Landing.tsx
+++ client/src/containers/Landing.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 import gt300AB from "assets/images/ovens/gt300AB.png";
 import gt600AB from "assets/images/ovens/gt600AB.png";
@@ -14,9 +15,30 @@ interface ILanding {}
 function Landing({ ...props }: ILanding) {
   return (
     <>
-      <section id="landing"></section>
+      <section id="main-company">
+        <div className="bd-container">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sint
+          illum hic nihil excepturi qui impedit voluptas, nulla cumque officia
+          quia veniam nemo aperiam officiis vel doloremque eligendi voluptate
+          alias. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
+          ut omnis voluptate iste sit velit quas, atque, numquam, vel ad quidem
+          ratione fugit minima tempora. Exercitationem laudantium quibusdam
+          nostrum neque. Lorem ipsum dolor sit amet consectetur, adipisicing
+          elit. Veniam vero corporis odio culpa iste id odit, minus dignissimos
+          laudantium illum. Eaque tenetur possimus magnam deleniti animi
+          molestias incidunt aliquam et.
+        </div>
+      </section>
+      <section id="main-product"></section>
+      <section id="main-contact"></section>
     </>
   );
 }
 
 export default Landing;
+
+const Wrapper = styled.div`
+  margin: 100px 320px 0 320px;
+`;
+
+const Banner = styled.div``;
