b96a2046e88858cc1d5edfc7e45f29c66e10a9aa
Oizys18 <oizys18@gmail.com>
2021-04-13 23:19:44 +0900

    Mod style imports

--- client/src/assets/styles/GlobalStyles.ts
+++ client/src/assets/styles/GlobalStyles.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700;900&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Heebo:wght@700&display=swap');
     :root {
